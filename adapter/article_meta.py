04aec4ca3c4b9531a192bf4542b09152768c98d1
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-02-25 15:56:03 -0300

    Melhora obtenção de campos do ArticleMeta

--- adapter/article_meta.py
+++ adapter/article_meta.py
@@ -1,13 +1,30 @@
 from xylose.scielodocument import Citation, Article
 
 
+def _replace_none(data):
+    return data if data else ''
+
+
 def to_doc_attrs(doc: Article):
-    return {'pid': doc.publisher_id,
-            'start_page': doc.start_page,
-            'end_page': doc.end_page,
-            'online_publication_date': doc.document_publication_date,
-            'issue_publication_date': doc.issue_publication_date,
-            'original_title': doc.original_title()}
+    attrs = {}
+
+    keys_props = ['collection_acronym',
+                  'publisher_id',
+                  'start_page',
+                  'end_page',
+                  'document_publication_date',
+                  'issue_publication_date',
+                  'first_author']
+
+    keys_funcs = ['original_title']
+
+    for p in keys_props:
+        attrs[p] = _replace_none(doc.__getattribute__(p))
+
+    for f in keys_funcs:
+        attrs[f] = _replace_none(doc.__getattribute__(f)())
+
+    return attrs
 
 
 def to_citref_attrs(cited_reference: Citation):
