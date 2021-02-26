407ae3d10bf97eb4f33e6ba333129d717435e507
Rafael J P Damaceno <rafael.pezzuto@gmail.com>
2021-02-26 16:31:31 -0300

    Adiciona lib values
    Biblioteca para indicar quais são os campos a serem tratados, quais não
    devem ser tratados, entre outros.

---
+++ lib/values.py
@@ -0,0 +1,53 @@
+ARTICLEMETA_DOCUMENT_CLEANED_FIELDS = {
+    'end_page',
+    'first_author',
+    'original_title',
+    'publication_date',
+    'translated_title',
+    'start_page'
+}
+
+ARTICLEMETA_DOCUMENT_METHOD_FIELDS = {
+    'any_issn': 16,
+    'bibliographic_legends': 255,
+    'file_code': 64,
+    'fulltexts': 1024,
+    'html_url': 128,
+    'issue_url': 128,
+    'keywords': 2048,
+    'languages': 64,
+    'original_language': 8,
+    'original_section': 128,
+    'original_title': 1024,
+    'pdf_url': 128,
+    'translated_section': 128,
+    'translated_titles': 1024,
+    'xml_languages': 32
+}
+
+ARTICLEMETA_DOCUMENT_STR_FIELDS = {
+    'acceptance_date': 16,
+    'assets_code': 16,
+    'collection_acronym': 8,
+    'collection_name': 16,
+    'contract': 128,
+    'creation_date': 16,
+    'data_model_version': 8,
+    'document_type': 32,
+    'doi': 64,
+    'end_page': 16,
+    'issue_label': 16,
+    'issue_publication_date': 16,
+    'order': 16,
+    'processing_date': 16,
+    'project_name': 255,
+    'publication_date': 16,
+    'publisher_id': 32,
+    'receive_date': 16,
+    'review_date': 16,
+    'scielo_domain': 32,
+    'section_code': 16,
+    'start_page': 16,
+    'thesis_degree': 16,
+    'update_date': 16
+}
